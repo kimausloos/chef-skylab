0739222dfdb9138ba04acc59f4d68eef9bb93703
Roderik van der Veer <roderik@vanderveer.be>
2014-01-27 13:46:56 +0100

    Install Skylab phar

--- recipes/default-nophp.rb
+++ recipes/default-nophp.rb
@@ -9,8 +9,10 @@ include_recipe "applications::bash-completion"
 include_recipe "dotfiles::bash_it"
 include_recipe "dotfiles::inputrc"
 include_recipe "applications::rsync"
+include_recipe "applications::curl"
 
-execute "install skyab" do
+
+execute "install skylab" do
     command "curl -sS https://raw.github.com/Kunstmaan/skylab/master/installer | php && mv skylab.phar /usr/local/bin/skylab"
     not_if { ::File.exists?("/usr/local/bin/skylab") }
 end
